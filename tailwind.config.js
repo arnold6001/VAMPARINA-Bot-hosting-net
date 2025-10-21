aea81d76776a5de1255ba6f0e5bc8b4c9125314c
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-10-22 01:34:14 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'vamp-purple': '#8B00FF',
+        'vamp-dark': '#1a1a2e',
+        'vamp-bg': '#16213e',
+      },
+    },
+  },
+  plugins: [],
+}
